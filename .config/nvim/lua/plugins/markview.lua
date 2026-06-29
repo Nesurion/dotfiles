71b6d7140bddebde38e7cecdb9819e5b166e9a2f
Maik Schwan <maik.schwan@gmail.com>
2026-06-29 10:21:56 +0200

    nvim: add markview plugin

---
+++ .config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,11 @@
+return {
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+}
