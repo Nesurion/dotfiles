2b7c24461a913e889bee155210eff2be202a5daf
Maik Schwan <maik.schwan@gmail.com>
2026-07-03 14:02:56 +0200

    nvim: diable snacks grep keymap (prefer fff)

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -32,6 +32,7 @@ return {
             list = {
               keys = {
                 ["<cr>"] = "confirm",
+                ["<leader>/"] = false,
               },
             },
           },
