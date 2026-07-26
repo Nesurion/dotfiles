6e793a8326f8ad63647919007d51a6e584f2a7a1
Maik Schwan <maik.schwan@leanix.net>
2026-07-26 13:41:52 +0200

    tmux: fix theme and active pane highlighting

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,25 +1,3 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-
--- Dim background when tmux focus is lost (works with tmux focus-events on)
-vim.api.nvim_create_autocmd("FocusLost", {
-  callback = function()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "#191926" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "#191926" })
-  end,
-})
-vim.api.nvim_create_autocmd("FocusGained", {
-  callback = function()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "#1e2030" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1e2030" })
-  end,
-})
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
@@ -27,3 +5,4 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.diagnostic.enable(false, { bufnr = args.buf })
   end,
 })
+
