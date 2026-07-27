2a174c1c0f5dd575ab9076bc68e6527ed6dfef2c
Maik Schwan <maik.schwan@gmail.com>
2026-07-27 07:49:26 +0200

    nvim: dim inactive pane (tokyonight colors)

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,4 +1,20 @@
 
+-- Dim nvim background when pane loses focus so tmux inactive-pane dimming works.
+-- Requires `set -g focus-events on` in tmux.conf (already set).
+vim.api.nvim_create_autocmd("FocusLost", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#2d3149" }) -- @thm_surface
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "#2d3149" })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FocusGained", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#222436" }) -- @thm_core
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "#222436" })
+  end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function(args)
