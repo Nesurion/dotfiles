55ebc43c75bc3205811e8bd3b9dc23c33fdc636d
Maik Schwan <maik.schwan@gmail.com>
2026-07-27 15:27:25 +0200

    nvim: disable snacks explore keymaps

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,4 +3,10 @@ return {
   opts = {
     explorer = { enabled = false },
   },
+  keys = {
+    { "<leader>e", false },
+    { "<leader>E", false },
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+  },
 }
